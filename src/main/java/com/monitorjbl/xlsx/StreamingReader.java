988bd1fad241969eba42b49da79b9f45ea32228f
Ivan Bell <ivanbell@yahoo.com>
2015-09-11 20:07:42 -0500

    Fix to consider cell styles
    Original solution skipped the styles, which caused date fields to be
    returned as days since epoch time, instead of a formatted date.  There
    was no way to determine which cells were actual numbers and which were
    dates (w/o assuming).
    
    Found a solution in
    http://poi.apache.org/spreadsheet/how-to.html#xssf_sax_api, specifically
    a link to
    https://svn.apache.org/repos/asf/poi/trunk/src/examples/src/org/apache/poi/xssf/eventusermodel/XLSX2CSV.java.
    Adapted that solution to your's to no longer ignore cell styles.
    
    There is probably a more elegant solution, using the StreamingCell's
    getCellType(), getStringCellValue(), getDateCellValue(), et. al.; but I
    had to get this working in < 1 day.  So, I offer this as a temporary fix
    until a more elegant solution can be found for using the other
    StreamingCell methods/fields in the StreamingReader.

--- src/main/java/com/monitorjbl/xlsx/StreamingReader.java
+++ src/main/java/com/monitorjbl/xlsx/StreamingReader.java
@@ -1,11 +1,14 @@
 package com.monitorjbl.xlsx;
 
+import org.apache.poi.ss.usermodel.BuiltinFormats;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import com.monitorjbl.xlsx.exceptions.CloseException;
 import com.monitorjbl.xlsx.exceptions.MissingSheetException;
 import com.monitorjbl.xlsx.exceptions.OpenException;
 import com.monitorjbl.xlsx.exceptions.ReadException;
 import com.monitorjbl.xlsx.impl.StreamingCell;
 import com.monitorjbl.xlsx.impl.StreamingRow;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
@@ -13,6 +16,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.eventusermodel.XSSFReader;
 import org.apache.poi.xssf.model.SharedStringsTable;
+import org.apache.poi.xssf.model.StylesTable;
 import org.apache.poi.xssf.usermodel.XSSFRichTextString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -62,7 +66,19 @@ public class StreamingReader implements Iterable<Row>, AutoCloseable {
 
   private File tmp;
 
-  private StreamingReader(SharedStringsTable sst, XMLEventReader parser, int rowCacheSize) {
+   /**
+    * <CODE>StylesTable</CODE> used to determine how to format numeric cell
+    * values.
+    */
+   private StylesTable stylesTable = null;
+
+   /**
+    * <CODE>DataFormatter</CODE> used to format numeric cell values.
+    */
+   private final DataFormatter dataFormatter = new DataFormatter();
+
+  private StreamingReader(StylesTable styles, SharedStringsTable sst, XMLEventReader parser, int rowCacheSize) {
+     this.stylesTable = styles;
     this.sst = sst;
     this.parser = parser;
     this.rowCacheSize = rowCacheSize;
@@ -100,7 +116,7 @@ public class StreamingReader implements Iterable<Row>, AutoCloseable {
     } else if (event.getEventType() == XMLStreamConstants.START_ELEMENT) {
       StartElement startElement = event.asStartElement();
       String tagLocalName = startElement.getName().getLocalPart();
-      
+
       if ("row".equals(tagLocalName)) {
         Attribute rowIndex = startElement.getAttributeByName(new QName("r"));
         currentRow = new StreamingRow(Integer.parseInt(rowIndex.getValue()));
@@ -110,12 +126,59 @@ public class StreamingReader implements Iterable<Row>, AutoCloseable {
         String[] coord = ref.getValue().split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
         currentCell = new StreamingCell(CellReference.convertColStringToIndex(coord[0]), Integer.parseInt(coord[1]) - 1);
 
-        Attribute type = startElement.getAttributeByName(new QName("t"));
-        if(type != null) {
-          currentCell.setType(type.getValue());
-        }
+         Attribute type = startElement.getAttributeByName(new QName("t"));
+/*ForgetMeNot--ibell--20150911--old code which didn't determine numeric vs date data types.
+         if(type != null) {
+            currentCell.setType(type.getValue());
+         }
+*/
+         //
+         // This block will determine the value type and set the appropriate
+         // XSSF data type (used to format numeric cell values).
+         //
+         final String typeValue = (type != null) ? type.getValue() : "";
+         if ("b".equals(typeValue)) {
+            currentCell.setXssfDataType(StreamingCell.XSSF_DATA_TYPE.BOOLEAN);
+         } else if ("e".equals(typeValue)) {
+            currentCell.setXssfDataType(StreamingCell.XSSF_DATA_TYPE.ERROR);
+         } else if ("str".equals(typeValue)) {
+            currentCell.setXssfDataType(StreamingCell.XSSF_DATA_TYPE.FORMULA);
+         } else if ("inlineStr".equals(typeValue)) {
+            currentCell.setXssfDataType(StreamingCell.XSSF_DATA_TYPE.INLINE_STRING);
+         } else if ("s".equals(typeValue)) {
+            currentCell.setXssfDataType(StreamingCell.XSSF_DATA_TYPE.SST_INDEX);
+
+         } else {
+            //
+            // The cell type is numeric, so need to determine and store the cell
+            // style and format.
+            //
+            currentCell.setXssfDataType(StreamingCell.XSSF_DATA_TYPE.NUMBER);
+
+            Attribute cellStyle = startElement.getAttributeByName(new QName("s"));
+            final String cellStyleString = (cellStyle != null) ? cellStyle.getValue() : null;
+
+            XSSFCellStyle style = null;
+
+            if (cellStyleString != null) {
+               style = stylesTable.getStyleAt(Integer.parseInt(cellStyleString));
+            } else if (stylesTable.getNumCellStyles() > 0) {
+               style = stylesTable.getStyleAt(0);
+            }
+
+            if (style != null) {
+               currentCell.setNumericFormatIndex(style.getDataFormat());
+               final String formatString = style.getDataFormatString();
+
+               if (formatString != null) {
+                  currentCell.setNumericFormatString(formatString);
+               } else {
+                  currentCell.setNumericFormatString(BuiltinFormats.getBuiltinFormat(currentCell.getNumericFormatIndex()));
+               }
+            }
+         }
       }
-      
+
       // Clear contents cache
       lastContents = "";
     } else if (event.getEventType() == XMLStreamConstants.END_ELEMENT) {
@@ -123,11 +186,69 @@ public class StreamingReader implements Iterable<Row>, AutoCloseable {
       String tagLocalName = endElement.getName().getLocalPart();
 
       if ("v".equals(tagLocalName)) {
+/*ForgetMeNot--ibell--20150911--old code which didn't determine numeric vs date data types.
         if ("s".equals(currentCell.getType())) {
           int idx = Integer.parseInt(lastContents);
           lastContents = new XSSFRichTextString(sst.getEntryAt(idx)).toString();
         }
         currentCell.setContents(lastContents);
+*/
+         //
+         // This block will format the numeric cell value based on the its XSSF
+         // data type.
+         //
+         String cellValue = null;
+         switch (currentCell.getXssfDataType()) {
+            case BOOLEAN:
+               char first = lastContents.charAt(0);
+               cellValue = first == '0' ? "FALSE" : "TRUE";
+               break;
+
+            case ERROR:
+               cellValue = "\"ERROR:  " + lastContents + '"';
+               break;
+
+            case FORMULA:
+               //
+               // A formula could result in a string value, so always add
+               // double-quote characters.
+               //
+               cellValue = '"' + lastContents + '"';
+               break;
+
+            case INLINE_STRING:
+               //
+               // have not seen an example of this, so it's untested.
+               //
+               XSSFRichTextString rtsi = new XSSFRichTextString(lastContents);
+               cellValue = '"' + rtsi.toString() + '"';
+               break;
+
+            case SST_INDEX:
+               try {
+                  int idx = Integer.parseInt(lastContents);
+                  XSSFRichTextString rtss = new XSSFRichTextString(this.sst.getEntryAt(idx));
+                  cellValue = '"' + rtss.toString() + '"';
+               } catch (java.lang.NumberFormatException nfe) {
+                  cellValue = "\"ERROR:  Failed to parse SST index '" + lastContents + "':  " + nfe.toString() + '"';
+               }
+               break;
+
+            case NUMBER:
+               final String formatString = currentCell.getNumericFormatString();
+               if (formatString != null && lastContents.length() > 0) {
+                  cellValue = this.dataFormatter.formatRawCellContents(Double.parseDouble(lastContents), currentCell.getNumericFormatIndex(), formatString);
+               } else {
+                  cellValue = lastContents;
+               }
+               break;
+
+            default:
+               cellValue = "\"ERROR:  Unexpected cell type:  " + currentCell.getXssfDataType() + '"';
+               break;
+         }
+
+         currentCell.setContents(cellValue);
       } else if ("row".equals(tagLocalName) && currentRow != null) {
         rowCache.add(currentRow);
       } else if ("c".equals(tagLocalName)) {
@@ -293,6 +414,7 @@ public class StreamingReader implements Iterable<Row>, AutoCloseable {
       try {
         OPCPackage pkg = OPCPackage.open(f);
         XSSFReader reader = new XSSFReader(pkg);
+        StylesTable styles = reader.getStylesTable();
         SharedStringsTable sst = reader.getSharedStringsTable();
 
         InputStream sheet = findSheet(reader);
@@ -301,7 +423,7 @@ public class StreamingReader implements Iterable<Row>, AutoCloseable {
         }
 
         XMLEventReader parser = XMLInputFactory.newInstance().createXMLEventReader(sheet);
-        return new StreamingReader(sst, parser, rowCacheSize);
+        return new StreamingReader(styles, sst, parser, rowCacheSize);
       } catch (IOException e) {
         throw new OpenException("Failed to open file", e);
       } catch (OpenXML4JException | XMLStreamException e) {
