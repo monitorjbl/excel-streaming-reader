1dc902c629bb8a08097d3e4607f7601ce25f3336
baihe liu <baihe liu>
2015-11-23 20:14:53 +0900

    fix #18

--- src/main/java/com/monitorjbl/xlsx/impl/StreamingCell.java
+++ src/main/java/com/monitorjbl/xlsx/impl/StreamingCell.java
@@ -148,7 +148,7 @@ public class StreamingCell implements Cell {
    */
   @Override
   public String getStringCellValue() {
-    return (String) contents;
+    return contents == null ? "" : (String) contents;
   }
 
   /**
