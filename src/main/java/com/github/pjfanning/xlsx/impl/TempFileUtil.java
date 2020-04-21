d043aa1b27daf5f3dc55d84510399adb11f2f40c
PJ Fanning <pj@kontainers.co.uk>
2020-04-22 00:28:28 +0200

    delete temp file if there is an issue

--- src/main/java/com/github/pjfanning/xlsx/impl/TempFileUtil.java
+++ src/main/java/com/github/pjfanning/xlsx/impl/TempFileUtil.java
@@ -1,5 +1,8 @@
 package com.github.pjfanning.xlsx.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -7,18 +10,27 @@ import java.io.InputStream;
 import java.nio.file.Files;
 
 public class TempFileUtil {
- public static File writeInputStreamToFile(InputStream is, int bufferSize) throws IOException {
-   if (is == null) throw new NullPointerException("InputStream is null");
-   File f = Files.createTempFile("tmp-", ".xlsx").toFile();
-   try(FileOutputStream fos = new FileOutputStream(f)) {
-     int read;
-     byte[] bytes = new byte[bufferSize];
-     while((read = is.read(bytes)) != -1) {
-       fos.write(bytes, 0, read);
-     }
-     return f;
-   } finally {
-     is.close();
-   }
- }
+  private static final Logger log = LoggerFactory.getLogger(TempFileUtil.class);
+
+  public static File writeInputStreamToFile(InputStream is, int bufferSize) throws IOException {
+    if (is == null) throw new NullPointerException("InputStream is null");
+    File f = Files.createTempFile("tmp-", ".xlsx").toFile();
+    try (FileOutputStream fos = new FileOutputStream(f)) {
+      int read;
+      byte[] bytes = new byte[bufferSize];
+      while ((read = is.read(bytes)) != -1) {
+        fos.write(bytes, 0, read);
+      }
+      return f;
+    } catch (IOException | RuntimeException | Error e) {
+      try {
+        f.delete();
+      } catch (Throwable t) {
+        log.warn("Failed to delete temp file {}: {}", f.getAbsolutePath(), t.toString());
+      }
+      throw e;
+    } finally {
+      is.close();
+    }
+  }
 }
