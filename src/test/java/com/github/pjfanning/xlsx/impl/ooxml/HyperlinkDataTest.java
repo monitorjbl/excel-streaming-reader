8fd36d352b8d041edfd05e0215f587cb2269d2ff
PJ Fanning <pjfanning@users.noreply.github.com>
2022-03-25 12:16:04 +0100

    Update HyperlinkDataTest.java

--- src/test/java/com/github/pjfanning/xlsx/impl/ooxml/HyperlinkDataTest.java
+++ src/test/java/com/github/pjfanning/xlsx/impl/ooxml/HyperlinkDataTest.java
@@ -59,10 +59,34 @@ public class HyperlinkDataTest {
     HyperlinkData hd1 = new HyperlinkData("id1", "ref1", "locn1", "disp1", "tip1");
     HyperlinkData hd2 = new HyperlinkData("id1", "ref1", "locn1", "disp1", null);
     HyperlinkData hd3 = new HyperlinkData("id1", "ref1", "locn1", "disp1", "tip2");
+    HyperlinkData hd4 = new HyperlinkData("id1", "ref1", "locn1", "disp2", "tip1");
+    HyperlinkData hd5 = new HyperlinkData("id1", "ref1", "locn1", null, "tip1");
+    HyperlinkData hd6 = new HyperlinkData("id1", "ref1", "locn2", "disp1", "tip1");
+    HyperlinkData hd7 = new HyperlinkData("id1", "ref1", null, "disp1", "tip1");
+    HyperlinkData hd8 = new HyperlinkData("id1", "ref2", "locn1", "disp1", "tip1");
+    HyperlinkData hd9 = new HyperlinkData("id1", null, "locn1", "disp1", "tip1");
+    HyperlinkData hd10 = new HyperlinkData("id2", "ref1", "locn1", "disp1", "tip1");
+    HyperlinkData hd11 = new HyperlinkData(null, "ref1", "locn1", "disp1", "tip1");
     assertNotEquals(hd1, hd2);
     assertNotEquals(hd1, hd3);
+    assertNotEquals(hd1, hd4);
+    assertNotEquals(hd1, hd5);
+    assertNotEquals(hd1, hd6);
+    assertNotEquals(hd1, hd7);
+    assertNotEquals(hd1, hd8);
+    assertNotEquals(hd1, hd9);
+    assertNotEquals(hd1, hd10);
+    assertNotEquals(hd1, hd11);
     assertNotEquals(hd1.hashCode(), hd2.hashCode());
     assertNotEquals(hd1.hashCode(), hd3.hashCode());
+    assertNotEquals(hd1.hashCode(), hd4.hashCode());
+    assertNotEquals(hd1.hashCode(), hd5.hashCode());
+    assertNotEquals(hd1.hashCode(), hd6.hashCode());
+    assertNotEquals(hd1.hashCode(), hd7.hashCode());
+    assertNotEquals(hd1.hashCode(), hd8.hashCode());
+    assertNotEquals(hd1.hashCode(), hd9.hashCode());
+    assertNotEquals(hd1.hashCode(), hd10.hashCode());
+    assertNotEquals(hd1.hashCode(), hd11.hashCode());
   }
 
 }
